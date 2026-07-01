fe81a0561890c189c0df0823ff36c17e6eff70a6
unknown <hudenagyonbaba@gmail.com>
2026-07-01 22:32:29 +0200

    dinocards supabase2

--- src/screens/RegionLevel.js
+++ src/screens/RegionLevel.js
@@ -5,7 +5,7 @@ import { COLORS } from '../constants/colors';
 import { IMAGE_MAP } from '../constants/imageMap';
 import { playQuizSfx } from '../audio/audioSystem';
 
-import { adaptCreature } from '../../services/creaturesService';
+import { adaptCreature } from '../services/creaturesService';
 //import { groupByPackage, csomagToPackId, getRegionKeyFromEdu } from '../../services/packageUtils';
 //import { generateQuestions } from '../../services/questionGenerator';
 import { generateQuestions } from '../services/questionGenerator';
