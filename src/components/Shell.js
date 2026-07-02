bfead1b870d71e4eb49f69ea43bf531e82b0b998
unknown <hudenagyonbaba@gmail.com>
2026-07-02 19:08:26 +0200

    shell

--- src/components/Shell.js
+++ src/components/Shell.js
@@ -1,49 +1,31 @@
-import { View, StyleSheet, Platform, useWindowDimensions } from 'react-native';
-import AdSenseSlot, { AD_SLOT_LEFT, AD_SLOT_RIGHT } from '../../AdSenseSlot';
-
-export default function Shell({ children }) {
-  const { width } = useWindowDimensions();
-  const showAdSlots = Platform.OS === 'web' && width >= 900;
-  const isWideWeb = Platform.OS === 'web' && width >= 700;
-
-  return (
-    <View style={styles.shellOuter}>
-      {showAdSlots && (
-        <View style={styles.adSlot}>
-          <AdSenseSlot slotId={AD_SLOT_LEFT} />
-        </View>
-      )}
-      <View style={[styles.shellInner, isWideWeb && styles.shellInnerWide]}>
-        {children}
-      </View>
-      {showAdSlots && (
-        <View style={styles.adSlot}>
-          <AdSenseSlot slotId={AD_SLOT_RIGHT} />
-        </View>
-      )}
-    </View>
-  );
-}
-
 const styles = StyleSheet.create({
   shellOuter: {
     flex: 1,
     width: '100%',
-    minHeight: '100%',
     backgroundColor: '#283618',
+
+    // FIX: remove minHeight, remove justifyContent:center
+    // Weben ez okozza a 0px magasságot
+    // minHeight: '100%',
+    // justifyContent: 'center',
+
     alignItems: 'stretch',
-    justifyContent: 'center',
     flexDirection: 'row',
   },
+
   shellInner: {
     flex: 1,
     width: '100%',
     maxWidth: 480,
-    minHeight: '100%',
+
+    // FIX: remove minHeight
+    // minHeight: '100%',
   },
+
   shellInnerWide: {
     maxWidth: 720,
   },
+
   adSlot: {
     flex: 1,
     minWidth: 120,
